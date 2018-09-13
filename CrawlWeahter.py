e77b8d3247b24a4c28a12d1057423c24bd2ab8c5
mahonghui <amisher@163.com>
2018-09-13 15:18:44 +0800

    push results to device

--- CrawlWeahter.py
+++ CrawlWeahter.py
@@ -3,7 +3,8 @@
 import requests
 import re
 from bs4 import BeautifulSoup as bs 
-import traceback
+
+from pushover import PushIt
 
 
 def getHTMLText(url):
@@ -48,13 +49,45 @@ def printInfo(Infolist):
 		item = Infolist[i]
 		print(header.format(item[0], item[1], item[2], chr(12288)))
 
+
+def push_messages(Info_list):
+
+	api_temp = 'https://api.pushover.net/1/messages.json?user={user}&token={token}&message={msg}'
+	user = 'un7nwysd2ma32rvjpwzvmsbzck2iqv'
+	token = 'aktaskhj8ygqgwd4tkwjsxwa6cgkyd'
+
+	header = '{0:^5}\t{1:^5}\t{2:^5}'
+	message = header.format('日期', '天气', '温度') + '\n'
+	for weather in Info_list:
+		message += header.format(weather[0], weather[1], weather[2]) + '\n'
+
+	# push message to my device
+	my_push = PushIt()
+	my_push.push(message)
+
+# 	full_api = api_temp.format(
+# 			user= user,
+# 			token= token,
+# 			msg = message
+# 		)
+# 	return full_api
+
+# push single message
+
+# def push_message(message):
+# 	r = requests.post(message)
+
 def main():
 	url = 'http://www.weather.com.cn/weather/101191301.shtml'
 	Infolist = []
 
 	html = getHTMLText(url)
 	parseWeather(Infolist, html)
-	printInfo(Infolist)
+	# printInfo(Infolist)
+
+	msg = push_messages(Infolist)
+	# push_message(msg)
+
 
 
 if __name__ == '__main__':
